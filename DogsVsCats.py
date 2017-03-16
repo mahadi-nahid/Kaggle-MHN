38fa2790347b01c563067c7e6783fecc4c8f0cb2
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2017-03-16 17:19:15 +0600

    fully implemented

--- DogsVsCats.py
+++ DogsVsCats.py
@@ -1,8 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 16 00:05:11 2017
+
+@author: NAHID
+
+https://pythonprogramming.net/convolutional-neural-network-kats-vs-dogs-machine-learning-tutorial/
+
+"""
+
+#-----------------------------------------------------------------------------------
 import cv2                 # working with, mainly resizing, images
 import numpy as np         # dealing with arrays
 import os                  # dealing with directories
 from random import shuffle # mixing up or currently ordered data that might lead our network astray in training.
 from tqdm import tqdm      # a nice pretty percentage bar for tasks. Thanks to viewer Daniel BA1/4hler for this suggestion
+
+#-----------------------------------------------------------------------------------
 
 TRAIN_DIR = 'X:/Kaggle_Data/dogs_vs_cats/train/train'
 TEST_DIR = 'X:/Kaggle_Data/dogs_vs_cats/test/test'
@@ -11,8 +24,7 @@ LR = 1e-3
 
 MODEL_NAME = 'dogsvscats-{}-{}.model'.format(LR, '2conv-basic') # just so we remember which saved model is which, sizes must match
 
-
-
+#-----------------------------------------------------------------------------------
 
 def label_img(img):
     word_label = img.split('.')[-3]
@@ -22,7 +34,7 @@ def label_img(img):
     #                             [no cat, very doggo]
     elif word_label == 'dog': return [0,1]
 
-	
+#-----------------------------------------------------------------------------------	
 	
 def create_train_data():
     training_data = []
@@ -36,8 +48,8 @@ def create_train_data():
     np.save('train_data.npy', training_data)
     return training_data
 
+#-----------------------------------------------------------------------------------
 
-	
 def process_test_data():
     testing_data = []
     for img in tqdm(os.listdir(TEST_DIR)):
@@ -51,14 +63,13 @@ def process_test_data():
     np.save('test_data.npy', testing_data)
     return testing_data
 
+#-----------------------------------------------------------------------------------
 	
 train_data = create_train_data()
 # If you have already created the dataset:
 #train_data = np.load('train_data.npy')
 
-
-
-
+#-----------------------------------------------------------------------------------
 
 import tflearn
 from tflearn.layers.conv import conv_2d, max_pool_2d
@@ -87,8 +98,8 @@ if os.path.exists('{}.meta'.format(MODEL_NAME)):
     model.load(MODEL_NAME)
     print('model loaded!')
 
+#-----------------------------------------------------------------------------------
 	
-
 train = train_data[:-500]
 test = train_data[-500:]
 
@@ -103,14 +114,102 @@ test_y = [i[1] for i in test]
 model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}), 
     snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
 
-
-	
-	
-
+#-----------------------------------------------------------------------------------
 	
 import tensorflow as tf
 tf.reset_default_graph()
 
-
 #size matters - bigger network
 
+#--------------------------------------------------------------
+convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 128, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 64, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = conv_2d(convnet, 32, 5, activation='relu')
+convnet = max_pool_2d(convnet, 5)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir='log')
+
+
+
+if os.path.exists('C:/Users/H/Desktop/KaggleDogsvsCats/{}.meta'.format(MODEL_NAME)):
+    model.load(MODEL_NAME)
+    print('model loaded!')
+
+train = train_data[:-500]
+test = train_data[-500:]
+
+X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y = [i[1] for i in train]
+
+test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+test_y = [i[1] for i in test]
+
+model.fit({'input': X}, {'targets': Y}, n_epoch=10, validation_set=({'input': test_x}, {'targets': test_y}), 
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+model.save(MODEL_NAME)
+
+#------------------------------------------------------------
+import matplotlib.pyplot as plt
+
+# if you need to create the data:
+#test_data = process_test_data()
+# if you already have some saved:
+test_data = np.load('test_data.npy')
+
+fig=plt.figure()
+
+for num,data in enumerate(test_data[:12]):
+    # cat: [1,0]
+    # dog: [0,1]
+    
+    img_num = data[1]
+    img_data = data[0]
+    
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    #model_out = model.predict([data])[0]
+    model_out = model.predict([data])[0]
+    
+    if np.argmax(model_out) == 1: str_label='Dog'
+    else: str_label='Cat'
+        
+    y.imshow(orig,cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
+
+#----------------------------------------------------------------------
+with open('submission_file.csv','w') as f:
+    f.write('id,label\n')
+            
+with open('submission_file.csv','a') as f:
+    for data in tqdm(test_data):
+        img_num = data[1]
+        img_data = data[0]
+        orig = img_data
+        data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+        model_out = model.predict([data])[0]
+        f.write('{},{}\n'.format(img_num,model_out[1]))
+		
+#----------------------------------------------------------------------
